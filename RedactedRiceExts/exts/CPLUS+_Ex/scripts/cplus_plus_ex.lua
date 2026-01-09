7b6b8df9ecf224059c453e3c7726a7973ff7c688
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-01-08 22:30:39 -0500

    Updates and fixes from testing

--- RedactedRiceExts/exts/CPLUS+_Ex/scripts/cplus_plus_ex.lua
+++ RedactedRiceExts/exts/CPLUS+_Ex/scripts/cplus_plus_ex.lua
@@ -15,80 +15,84 @@ local PER_PILOT = cplus_plus_ex.REUSABLILITY.PER_PILOT
 cplus_plus_ex.DEFAULT_REUSABILITY = PER_PILOT
 cplus_plus_ex.DEFAULT_WEIGHT = 1.0
 cplus_plus_ex.VANILLA_SKILLS = {
-	{id = "Health", shortName = "Pilot_HealthShort", fullName = "Pilot_HealthName", description= "Pilot_HealthDesc", bonuses = {health = 2}, saveVal = 0, reusability = REUSABLE },
+	{id = "Health", shortName = "Pilot_HealthShort", fullName = "Pilot_HealthName", description= "Pilot_HealthDesc", bonuses = {health = 2}, saveVal = 0, reusability = REUSABLE }, 
 	{id = "Move", shortName = "Pilot_MoveShort", fullName = "Pilot_MoveName", description= "Pilot_MoveDesc", bonuses = {move = 1}, saveVal = 1, reusability = REUSABLE },
-	{id = "Grid", shortName = "Pilot_GridShort", fullName = "Pilot_GridName", description= "Pilot_GridDesc", bonuses = {grid = 3}, saveVal = 2, reusability = REUSABLE },
-	{id = "Reactor", shortName = "Pilot_ReactorShort", fullName = "Pilot_ReactorName", description= "Pilot_ReactorDesc", bonuses = {cores = 1}, saveVal = 3, reusability = REUSABLE },
-	{id = "Opener", shortName = "Pilot_OpenerName", fullName = "Pilot_OpenerName", description= "Pilot_OpenerDesc", saveVal = 4, reusability = PER_PILOT },
-	{id = "Closer", shortName = "Pilot_CloserName", fullName = "Pilot_CloserName", description= "Pilot_CloserDesc", saveVal = 5, reusability = PER_PILOT },
-	{id = "Popular", shortName = "Pilot_PopularName", fullName = "Pilot_PopularName", description= "Pilot_PopularDesc", saveVal = 6, reusability = PER_PILOT },
-	{id = "Thick", shortName = "Pilot_ThickName", fullName = "Pilot_ThickName", description= "Pilot_ThickDesc", saveVal = 7, reusability = PER_PILOT },
+	{id = "Grid", shortName = "Pilot_GridShort", fullName = "Pilot_GridName", description= "Pilot_GridDesc", bonuses = {grid = 3}, saveVal = 2, reusability = PER_PILOT }, -- only registers one grid for some reason
+	{id = "Reactor", shortName = "Pilot_ReactorShort", fullName = "Pilot_ReactorName", description= "Pilot_ReactorDesc", bonuses = {cores = 1}, saveVal = 3, reusability = PER_PILOT }, -- only registers one for some reason
+	{id = "Opener", shortName = "Pilot_OpenerName", fullName = "Pilot_OpenerName", description= "Pilot_OpenerDesc", saveVal = 4, reusability = PER_PILOT }, -- doesn't work
+	{id = "Closer", shortName = "Pilot_CloserName", fullName = "Pilot_CloserName", description= "Pilot_CloserDesc", saveVal = 5, reusability = PER_PILOT }, -- doesn't work
+	{id = "Popular", shortName = "Pilot_PopularName", fullName = "Pilot_PopularName", description= "Pilot_PopularDesc", saveVal = 6, reusability = PER_PILOT }, -- doesn't work
+	{id = "Thick", shortName = "Pilot_ThickName", fullName = "Pilot_ThickName", description= "Pilot_ThickDesc", saveVal = 7, reusability = PER_PILOT }, -- doesn't make sense
 	{id = "Skilled", shortName = "Pilot_SkilledName", fullName = "Pilot_SkilledName", description= "Pilot_SkilledDesc", bonuses = {health = 2, move = 1}, saveVal = 8, reusability = REUSABLE },
-	{id = "Invulnerable", shortName = "Pilot_InvulnerableName", fullName = "Pilot_InvulnerableName", description= "Pilot_InvulnerableDesc", saveVal = 9, reusability = REUSABLE },
-	{id = "Adrenaline", shortName = "Pilot_AdrenalineName", fullName = "Pilot_AdrenalineName", description= "Pilot_AdrenalineDesc", saveVal = 10, reusability = PER_PILOT },
-	{id = "Pain", shortName = "Pilot_PainName", fullName = "Pilot_PainName", description= "Pilot_PainDesc", saveVal = 11, reusability = PER_PILOT },
-	{id = "Regen", shortName = "Pilot_RegenName", fullName = "Pilot_RegenName", description= "Pilot_RegenDesc", saveVal = 12, reusability = PER_PILOT },
-	{id = "Conservative", shortName = "Pilot_ConservativeName", fullName = "Pilot_ConservativeName", description= "Pilot_ConservativeDesc", saveVal = 13, reusability = PER_PILOT },
+	{id = "Invulnerable", shortName = "Pilot_InvulnerableName", fullName = "Pilot_InvulnerableName", description= "Pilot_InvulnerableDesc", saveVal = 9, reusability = PER_PILOT }, -- doesn't make sense
+	{id = "Adrenaline", shortName = "Pilot_AdrenalineName", fullName = "Pilot_AdrenalineName", description= "Pilot_AdrenalineDesc", saveVal = 10, reusability = PER_PILOT }, -- doesn't work
+	{id = "Pain", shortName = "Pilot_PainName", fullName = "Pilot_PainName", description= "Pilot_PainDesc", saveVal = 11, reusability = PER_PILOT }, -- doesn't work
+	{id = "Regen", shortName = "Pilot_RegenName", fullName = "Pilot_RegenName", description= "Pilot_RegenDesc", saveVal = 12, reusability = PER_PILOT }, -- doesn't work
+	{id = "Conservative", shortName = "Pilot_ConservativeName", fullName = "Pilot_ConservativeName", description= "Pilot_ConservativeDesc", saveVal = 13, reusability = PER_PILOT }, -- doesn't work
 }
 
--- Load modules
-local utils = require("scripts/utils")
-local skill_registry = require("scripts/skill_registry")
-local skill_config = require("scripts/skill_config")
-local skill_selection = require("scripts/skill_selection")
-local skill_constraints = require("scripts/skill_constraints")
-local time_traveler = require("scripts/time_traveler")
-
-cplus_plus_ex._modules = {
-	skill_config = skill_config,
-	skill_constraints = skill_constraints,
-	skill_registry = skill_registry,
-	skill_selection = skill_selection,
-	time_traveler = time_traveler,
-	utils = utils,
-}
+local utils = nil
+local skill_registry = nil
+local skill_config = nil
+local skill_selection = nil
+local skill_constraints = nil
+local time_traveler = nil
 
 -- Initialize modules (called from init function)
-function cplus_plus_ex:initModules()
+function cplus_plus_ex:initModules(path)
+	utils = require(path.."scripts/utils")
+	skill_registry = require(path.."scripts/skill_registry")
+	skill_config = require(path.."scripts/skill_config")
+	skill_selection = require(path.."scripts/skill_selection")
+	skill_constraints = require(path.."scripts/skill_constraints")
+	time_traveler = require(path.."scripts/time_traveler")
+	
+	self._modules = {
+		skill_config = skill_config,
+		skill_constraints = skill_constraints,
+		skill_registry = skill_registry,
+		skill_selection = skill_selection,
+		time_traveler = time_traveler,
+		utils = utils,
+	}
+	
 	skill_config.init(self)
 	skill_constraints.init(self)
 	skill_registry.init(self)
 	skill_selection.init(self)
 	time_traveler.init(self)
+	
+	-- Expose "public" module params/functions/classes APIs
+	cplus_plus_ex.config = skill_config.config
+	cplus_plus_ex.SkillConfig = skill_config.SkillConfig
+
+	function cplus_plus_ex:setSkillConfig(...) return skill_config.setSkillConfig(...) end
+	function cplus_plus_ex:enableSkill(...) return skill_config.enableSkill(...) end
+	function cplus_plus_ex:disableSkill(...) return skill_config.disableSkill(...) end
+	function cplus_plus_ex:removeSkillDependency(...) return skill_config.removeSkillDependency(...) end
+	function cplus_plus_ex:setAdjustedWeightsConfigs() return skill_config.setAdjustedWeightsConfigs() end
+	function cplus_plus_ex:resetToDefaults() return skill_config.resetToDefaults() end
+	function cplus_plus_ex:getAllowedReusability(...) return skill_config.getAllowedReusability(...) end
+
+	function cplus_plus_ex:checkSkillConstraints(...) return skill_constraints.checkSkillConstraints(...) end
+	function cplus_plus_ex:registerConstraintFunction(...) return skill_constraints.registerConstraintFunction(...) end
+
+	function cplus_plus_ex:registerSkill(...) return skill_registry.registerSkill(...) end
+	function cplus_plus_ex:registerPilotSkillExclusions(...) return skill_registry.registerPilotSkillExclusions(...) end
+	function cplus_plus_ex:registerPilotSkillInclusions(...) return skill_registry.registerPilotSkillInclusions(...) end
+	function cplus_plus_ex:registerSkillExclusion(...) return skill_registry.registerSkillExclusion(...) end
+	function cplus_plus_ex:registerSkillDependency(...) return skill_registry.registerSkillDependency(...) end
+
+	function cplus_plus_ex:applySkillsToPilot(...) return skill_selection.applySkillsToPilot(...) end
+	function cplus_plus_ex:applySkillsToAllPilots() return skill_selection.applySkillsToAllPilots() end
+
+	-- Wrapper for time_traveler since we can't do a ref as we reassign the ref each time we find the time traveler
+	function cplus_plus_ex:getTimeTraveler() return time_traveler.timeTraveler end
 end
 
--- Expose "public" module params/functions/classes APIs
-cplus_plus_ex.config = skill_config.config
-cplus_plus_ex.SkillConfig = skill_config.SkillConfig
-
--- Expose the public facing APIs
-function cplus_plus_ex:setSkillConfig(...) return skill_config.setSkillConfig(...) end
-function cplus_plus_ex:enableSkill(...) return skill_config.enableSkill(...) end
-function cplus_plus_ex:disableSkill(...) return skill_config.disableSkill(...) end
-function cplus_plus_ex:removeSkillDependency(...) return skill_config.removeSkillDependency(...) end
-function cplus_plus_ex:setAdjustedWeightsConfigs() return skill_config.setAdjustedWeightsConfigs() end
-function cplus_plus_ex:resetToDefaults() return skill_config.resetToDefaults() end
-function cplus_plus_ex:getAllowedReusability(...) return skill_config.getAllowedReusability(...) end
-
-function cplus_plus_ex:checkSkillConstraints(...) return skill_constraints.checkSkillConstraints(...) end
-function cplus_plus_ex:registerConstraintFunction(...) return skill_constraints.registerConstraintFunction(...) end
-
-function cplus_plus_ex:registerSkill(...) return skill_registry.registerSkill(...) end
-function cplus_plus_ex:registerPilotSkillExclusions(...) return skill_registry.registerPilotSkillExclusions(...) end
-function cplus_plus_ex:registerPilotSkillInclusions(...) return skill_registry.registerPilotSkillInclusions(...) end
-function cplus_plus_ex:registerSkillExclusion(...) return skill_registry.registerSkillExclusion(...) end
-function cplus_plus_ex:registerSkillDependency(...) return skill_registry.registerSkillDependency(...) end
-
-function cplus_plus_ex:applySkillsToPilot(...) return skill_selection.applySkillsToPilot(...) end
-function cplus_plus_ex:applySkillsToAllPilots() return skill_selection.applySkillsToAllPilots() end
-
--- Wrapper for time_traveler since we can't do a ref as we reassign the ref each time we find the time traveler
-function cplus_plus_ex:getTimeTraveler() return time_traveler.timeTraveler end
-
-
-function cplus_plus_ex:init()
+function cplus_plus_ex:init(path)
 	-- Initialize modules
-	self:initModules()
+	self:initModules(path)
 
 	-- Add events
 	self:addEvents()
