d0267b86e1d25748819dd5ea17682ff9fc610318
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2026-04-22 22:30:13 -0400

    Adding squad based exclusions

--- RedactedRiceExts/exts/CPLUS+_Ex/cplus_plus_ex.lua
+++ RedactedRiceExts/exts/CPLUS+_Ex/cplus_plus_ex.lua
@@ -158,6 +158,8 @@ function cplus_plus_ex:exposeAPI()
 	function cplus_plus_ex:registerSkill(...) return skill_registry:registerSkill(...) end
 	function cplus_plus_ex:registerPilotSkillExclusions(...) return skill_registry:registerPilotSkillExclusions(...) end
 	function cplus_plus_ex:registerPilotSkillInclusions(...) return skill_registry:registerPilotSkillInclusions(...) end
+	function cplus_plus_ex:registerSquadSkillExclusions(...) return skill_registry:registerSquadSkillExclusions(...) end
+	function cplus_plus_ex:registerSquadSkillInclusions(...) return skill_registry:registerSquadSkillInclusions(...) end
 	function cplus_plus_ex:registerSkillExclusion(...) return skill_registry:registerSkillExclusion(...) end
 	function cplus_plus_ex:registerSkillToGroup(...) return skill_registry:registerSkillToGroup(...) end
 
